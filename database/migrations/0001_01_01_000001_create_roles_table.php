0265d22470bae1433de942a1fa9b603e39637dc1
benaleo <benaleo.projects@gmail.com>
2025-05-14 10:26:05 +0700

    change db to use mysql

--- database/migrations/0001_01_01_000001_create_roles_table.php
+++ database/migrations/0001_01_01_000001_create_roles_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
@@ -9,7 +10,7 @@ return new class extends Migration {
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id()->primary();
-            $table->uuid('secure_id')->unique()->default(DB::raw('gen_random_uuid()'));
+            $table->uuid('secure_id')->unique()->default(\Illuminate\Support\Str::uuid());
             $table->string('name');
             $table->string('updated_by')->nullable();;
             $table->timestamps();
--- database/migrations/0001_01_01_000002_create_users_table.php
+++ database/migrations/0001_01_01_000002_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id()->primary();
-            $table->uuid('secure_id')->default(DB::raw('gen_random_uuid()'));
+            $table->uuid('secure_id')->default(\Illuminate\Support\Str::uuid());
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
